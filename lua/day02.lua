e8a4f4e10c34181f2b2e861133f9eeeaa9743437
Mindy Medeiros <mmedei6@wgu.edu>
2025-04-06 12:17:47 -0400

    Sample input and beginnings of day 2

---
+++ lua/day02.lua
@@ -0,0 +1,23 @@
+--[[
+The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if both of the following are true:
+
+    The levels are either all increasing or all decreasing.
+    Any two adjacent levels differ by at least one and at most three.
+]]
+
+
+-- Use curly braces to create a table literal
+local reports = {}
+
+-- Read lines, stolen from day 1
+-- Alternative option spelling out its a global
+-- for line in _G.io.lines() do
+for line in io.lines() do
+    -- something
+end
+
+
+-- The levels are either all increasing or all decreasing.
+
+
+-- Any two adjacent levels differ by at least one and at most three.
